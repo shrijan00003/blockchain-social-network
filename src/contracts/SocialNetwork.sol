0fa72b176e69e6680175851b996b3f57d9e08420
shrijan <shrijan00003@gmail.com>
2020-03-31 01:24:48 +0545

    - added mehtod and event for tipping in post - added test for tipping to existig post - validation added for rejecting if the id doesnot exist :

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -2,9 +2,68 @@ pragma solidity ^0.5.0;
 
 
 contract SocialNetwork {
+    // Data structure
     string public name;
+    uint256 public postCount = 0;
+    struct Post {
+        uint256 id;
+        string content;
+        uint256 tipAmount;
+        address payable author;
+    }
 
+    // Defining data structure posts to handle posts of Post type
+    // where key is id wiht uint256 data taype and value is Post
+    mapping(uint256 => Post) public posts;
+
+    // Events
+    event PostCreated(
+        uint256 id,
+        string content,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event PostTipped(
+        uint256 id,
+        string content,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    // Methods
     constructor() public {
         name = "LearnTechFree Social Network";
+    }
+
+    function createPost(string memory _content) public {
+        // require validation
+        require(bytes(_content).length > 0, "Content is Required");
+
+        postCount++;
+        posts[postCount] = Post(postCount, _content, 0, msg.sender);
+        emit PostCreated(postCount, _content, 0, msg.sender);
+    }
+
+    function tipPost(uint256 _id) public payable {
+        // Fetch the post
+        Post memory _post = posts[_id];
+
+        require(_post.id > 0, "Id must be in posts");
+
+        // Fetch the author
+        address payable _author = _post.author;
+
+        // pay the author by sending them ehter
+        address(_author).transfer(msg.value);
+
+        // Increase the tip ammount
+        _post.tipAmount = _post.tipAmount + msg.value;
+
+        // Update the post
+        posts[_id] = _post;
+
+        // Trigger an event
+        emit PostTipped(postCount, _post.content, _post.tipAmount, _author);
     }
 }
--- test/SocialNetwork.test.js
+++ test/SocialNetwork.test.js
@@ -3,7 +3,7 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-contract("Social Network", accounts => {
+contract("Social Network", ([deployer, author, tipper]) => {
   let socialNetworkContract;
   before(async () => {
     socialNetworkContract = await SocialNetwork.deployed();
@@ -19,6 +19,82 @@ contract("Social Network", accounts => {
     it("has a name", async () => {
       const name = await socialNetworkContract.name();
       assert.equal(name, "LearnTechFree Social Network");
+    });
+  });
+
+  describe("posts", () => {
+    let result, postCount;
+    const content = "This is my first article";
+    before(async () => {
+      result = await socialNetworkContract.createPost(content, {
+        from: author
+      });
+      postCount = await socialNetworkContract.postCount();
+    });
+    it("should create post ", async () => {
+      // SUCCESS
+      assert.equal(postCount, 1);
+
+      const event = result.logs[0].args;
+
+      assert.equal(event.id.toNumber(), postCount.toNumber(), "id is correct");
+      assert.equal(event.content, content, "Content is correct");
+      assert.equal(event.tipAmount, "0", "Tip amount is correct");
+      assert.equal(event.author, author, "Author is correct");
+
+      // FAILURE: Post must have content
+      await socialNetworkContract.createPost("", { from: author }).should.be
+        .rejected;
+    });
+
+    it("should list post", async () => {
+      const post = await socialNetworkContract.posts(postCount);
+      assert.equal(post.id.toNumber(), postCount.toNumber());
+      assert.equal(post.content, content, "Content is correct");
+      assert.equal(post.tipAmount, "0", "Tip amount is correct");
+      assert.equal(post.author, author, "Author is correct");
+    });
+
+    it("should allows users to tip posts", async () => {
+      // Track the author balance before purchase
+      let oldAuthorBalance;
+      oldAuthorBalance = await web3.eth.getBalance(author);
+      oldAuthorBalance = new web3.utils.BN(oldAuthorBalance);
+
+      result = await socialNetworkContract.tipPost(postCount, {
+        from: tipper,
+        value: web3.utils.toWei("1", "Ether")
+      });
+
+      // SUCCESS
+      const event = result.logs[0].args;
+
+      assert.equal(event.id.toNumber(), postCount.toNumber(), "id is correct");
+      assert.equal(event.content, content, "content is correct");
+      assert.equal(
+        event.tipAmount.toString(),
+        "1000000000000000000",
+        "tip amount is correct"
+      );
+      assert.equal(event.author, author, "author is correct");
+
+      // Check that author received funds
+      let newAuthorBalance;
+      newAuthorBalance = await web3.eth.getBalance(author);
+      newAuthorBalance = new web3.utils.BN(newAuthorBalance);
+
+      let tipAmount;
+      tipAmount = web3.utils.toWei("1", "Ether");
+      tipAmount = new web3.utils.BN(tipAmount);
+
+      const expectedBalance = oldAuthorBalance.add(tipAmount);
+
+      assert.equal(newAuthorBalance.toString(), expectedBalance.toString());
+      // FAILURE: Tries to tip a post that does not exist
+      await socialNetworkContract.tipPost(99, {
+        from: tipper,
+        value: web3.utils.toWei("1", "Ether")
+      }).should.be.rejected;
     });
   });
 });
