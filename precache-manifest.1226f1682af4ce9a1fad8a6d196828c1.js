64aca140c069d29547e24c7beab083f285e26750
shrijan <shrijan00003@gmail.com>
2020-04-01 23:37:06 +0545

    Updates

---
+++ precache-manifest.1226f1682af4ce9a1fad8a6d196828c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36144ce3a8eeb14504fd01520942038",
+    "url": "/blockchain-social-network/index.html"
+  },
+  {
+    "revision": "f1282fd9bb4cb940565e",
+    "url": "/blockchain-social-network/static/css/2.e15a0812.chunk.css"
+  },
+  {
+    "revision": "718f4aa1e8332e517128",
+    "url": "/blockchain-social-network/static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "f1282fd9bb4cb940565e",
+    "url": "/blockchain-social-network/static/js/2.c23436c5.chunk.js"
+  },
+  {
+    "revision": "6e1c88f1e9831c51f95f08358317ff1a",
+    "url": "/blockchain-social-network/static/js/2.c23436c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "718f4aa1e8332e517128",
+    "url": "/blockchain-social-network/static/js/main.56bfa2ab.chunk.js"
+  },
+  {
+    "revision": "9c637963f4c4f16899c1",
+    "url": "/blockchain-social-network/static/js/runtime-main.af802cc1.js"
+  }
+]);
